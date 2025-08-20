2fef11306310231d3f72e28431b2b720bc9b48c7
Ramiro Becker <ramirobecker321@gmail.com>
2025-08-20 16:42:54 -0300

    Creacion de repositorio de ejercicios

---
+++ app/src/main/java/com/ramiro/cfp401/cursoandroid/Ejercicios1.kt
@@ -0,0 +1,20 @@
+package com.ramiro.cfp401.cursoandroid
+
+fun main (){
+
+/*
+    private fun Ejercicio1(){
+    val Bochas = readln().toInt()
+    val Precio 100
+    var Pago :Float
+    fun CantidadBochas :Float
+
+    println("el precio es de $100 por bocha de helado, ¿cuantas quiere?")
+
+    fun Operacion () {
+        Bochas * Precio if Bochas >= 3 % 10 = Pago
+    }
+
+*/
+
+}
